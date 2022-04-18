f57ba778a92f4efc0367401825278a6bab3aa225
woorim99 <u_rim@naver.com>
2022-04-19 00:02:48 +0900

    데이터 db에 저장(save_toDB.py)

---
+++ Section 3/API_test.py
@@ -0,0 +1,60 @@
+import requests 
+
+id = "account.cacb55280bb84012bc04ac2999f76fee"
+woorim_url = "https://api.pubg.com/shards/kakao/players?filter[playerNames]=S2_Rimi"  #유저정보
+match_url = "https://api.pubg.com/shards/kakao/matches/968ae5a2-f75c-489e-88d8-19c4de609456"   #매치정보
+weapon_url = f"https://api.pubg.com/shards/kakao/players/{id}/weapon_mastery"     #무기 마스터리
+survival_url = f"https://api.pubg.com/shards/kakao/players/{id}/survival_mastery"  #딜평균 총량 등등
+lifetime_url = f"https://api.pubg.com/shards/kakao/players/{id}/seasons/lifetime?filter[gamepad]=false"         #매치타입별 생존시간
+
+header = { 
+    "Authorization": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyN2UxN2U4MC1hMTA5LTAxM2EtYzQ5OS0wZTVmZGQ2ODc3NTYiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNjUwMjYwOTM0LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Indvb19wMyJ9.9ee6mVz2tpkFlzb1P6QccxB3DwVHHa3DPu1zve4y690", 
+    "Accept": "application/vnd.api+json" 
+    }
+
+
+
+woorim_r = requests.get(woorim_url, headers=header)
+match_r = requests.get(match_url, headers=header)
+weapon_r = requests.get(weapon_url, headers=header)
+lifetime_r = requests.get(lifetime_url, headers=header)
+survival_r = requests.get(survival_url, headers=header)
+
+player_woorim_json = woorim_r.json()
+match_json = match_r.json()
+weapon_json = weapon_r.json()
+survival_json = survival_r.json()
+lifetime_json = lifetime_r.json()
+
+print(match_json)
+# print(player_woorim_json['data'][0]['relationships']['matches']['data'])
+match_list = []
+# for i in player_woorim_json['data'][0]['relationships']['matches']['data']:
+#     match_list.append(i['id'])
+#     # print(i['id'])
+# # print(match_list)
+
+# my_match_stat = []
+# for i in match_list:
+#     match_url = f"https://api.pubg.com/shards/kakao/matches/{i}"
+#     match_r = requests.get(match_url, headers=header)
+#     match_json = match_r.json()
+    
+#     for j in match_json['included']:              # 내아이디의 매치정보만 가져오기
+#         try: 
+#             if j['attributes']['stats']['name']=='S2_Rimi':
+#                 # print(i['attributes']['stats'])
+#                 my_match_stat.append(j['attributes']['stats'])
+#         except:
+#             pass
+
+# for i in match_json['included']:              # 내아이디의 매치정보만 가져오기
+#     try: 
+#         if i['attributes']['stats']['name']=='S2_Rimi':
+#             print(i['attributes']['stats'])
+#     except:
+#         pass
+    
+# print(weapon_json)
+# print(survival_json)
+# print(lifetime_json)
---
+++ Section 3/save_toDB.py
@@ -0,0 +1,51 @@
+import requests
+import sqlite3
+
+header = { 
+    "Authorization": "API key", 
+    "Accept": "application/vnd.api+json" 
+    }
+
+#player라는 닉네임의 유저가 최근 14일 내에 플레이 한 매치들을 리스트 형태로 반환
+def returnPlayerMatch(player):    
+    url = f"https://api.pubg.com/shards/kakao/players?filter[playerNames]={player}"
+    r = requests.get(url, headers=header)
+    player_json = r.json()
+    
+    match_list = []
+    for i in player_json['data'][0]['relationships']['matches']['data']:
+        match_list.append(i['id'])
+        
+    return match_list
+
+
+# match_list안의 매치 속 플레이어들의 스탯 data.db의 pubg_stat에 저장
+def returnUserStat(match_list):
+    user_stat = []
+    id = 0
+    for matchId in match_list:
+        match_url = f"https://api.pubg.com/shards/kakao/matches/{matchId}"
+        match_r = requests.get(match_url, headers=header)
+        match_json = match_r.json()
+        
+        conn = sqlite3.connect('data.db')
+        cur = conn.cursor()
+        
+        for i in match_json['included']:       
+            try: 
+                user_stat.append(i['attributes']['stats'])
+                kills = i['attributes']['stats']['kills']
+                damageDealt = i['attributes']['stats']['damageDealt']
+                totalDistance = i['attributes']['stats']['rideDistance'] + i['attributes']['stats']['swimDistance'] + i['attributes']['stats']['walkDistance']
+                winPlace = i['attributes']['stats']['winPlace']
+                
+                cur.execute(f"INSERT INTO pubg_stat VALUES ({id}, {kills}, {damageDealt}, {totalDistance}, {winPlace})")
+                id += 1
+            except:
+                pass
+            conn.commit()
+    return(id)  
+
+match_list = returnPlayerMatch('S2_Rimi') + returnPlayerMatch('glhboy') + returnPlayerMatch('17TAENG') + returnPlayerMatch('Js-o-zz') + returnPlayerMatch('choco_candy')
+print(returnUserStat(match_list))
+
